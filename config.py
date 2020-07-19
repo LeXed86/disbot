507a69b6e68b920721fe8d98019da1e8aade54f2
LeXed86 <ganster86@gmail.com>
2020-07-19 13:35:00 +0500

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-TOKEN = 'NzMxNzkzMDY0MTMzMzk0NDMy.Xwr1wQ.GeGrFhQu9OeB84K_SaNDnmYTRB4' # bot token
+TOKEN = '' # bot token
 
 POST_ID = 731804044053577790 # post id to read reactions from
 
@@ -6,7 +6,7 @@ ROLES = {
 		'🥇': 731797570577498152, # Gold Gamer
    		'🥈': 392563028421246976, # Silver Gamer
 		'🙃': 731797821677895700, # Noob
-	
+
 } # roles list according to emotes
 
 EXCROLES = () # exclude this roles from counting
